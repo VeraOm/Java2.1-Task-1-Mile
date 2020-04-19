d978e1d69116a5bf0094ab8e3254818661c01db7
VeraOm <aug_ooy@tutanota.com>
2020-04-19 17:24:09 +0600

    Исправление замечаний

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int ticket=110;
+        int ticket = 110;
         Scanner numb = new Scanner(System.in);
         System.out.print("Введите количество билетов: ");
         int num = numb.nextInt();
-        int mile=(int)Math.floor(num*ticket/20);
+        int mile = (int) Math.floor(num * ticket / 20);
         System.out.printf("Количество миль - %d", mile);
-	    numb.close();
+        numb.close();
     }
 }
