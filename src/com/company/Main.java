b026d97c757ecb48f490188f8614752e59b85aa3
VeraOm <aug_ooy@tutanota.com>
2020-04-19 13:03:56 +0600

    Корректировка отчета и программы

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int ticket=110;
         Scanner numb = new Scanner(System.in);
-        System.out.print("Ввыедите количество билетов: ");
+        System.out.print("Введите количество билетов: ");
         int num = numb.nextInt();
         int mile=(int)Math.floor(num*ticket/20);
         System.out.printf("Количество миль - %d", mile);
